0fcc26ae5334d21cec320cbfb02809654314b845
daniil <75467751+daniileontev@users.noreply.github.com>
2023-07-11 16:26:12 +0500

    add tests for without()

--- tests/UtilsTest.php
+++ tests/UtilsTest.php
@@ -5,6 +5,7 @@ namespace Hexlet\Phpunit\Tests;
 use PHPUnit\Framework\TestCase;
 
 use function Hexlet\Phpunit\Utils\reverseString;
+use function Hexlet\Phpunit\Utils\without;
 
 // класс UtilsTest наследует класс TestCase
 // имя класса совпадает с именем файла
@@ -19,5 +20,13 @@ class UtilsTest extends TestCase
         // И только потом актуальное (actual)
         $this->assertEquals('', reverseString(''));
         $this->assertEquals('olleh', reverseString('hello'));
+    }
+
+    public function testWithout(): void
+    {
+        // Сначала идет ожидаемое значение (expected)
+        // И только потом актуальное (actual)
+        $this -> assertEquals([3], without([2, 1, 2, 3], [1, 2]));
+        $this -> assertEquals([], without([]));
     }
 }
